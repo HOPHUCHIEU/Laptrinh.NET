fe408df2cbc6b3a9504d671cbc23d58a0c7103e8
HOPHUCHIEU <hophuchieu135@gmail.com>
2024-05-14 00:30:26 +0700

    doancanhan

---
+++ DoAnCaNhan/DoAnCaNhan/frm_DangNhap.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace DoAnCaNhan
+{
+    partial class frm_DangNhap
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.Button btn_DangNhap;
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frm_DangNhap));
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_TenDangNhap = new System.Windows.Forms.TextBox();
+            this.txt_MatKhau = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btn_Thoat = new System.Windows.Forms.Button();
+            this.ck_HienThiMatKhau = new System.Windows.Forms.CheckBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            btn_DangNhap = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_DangNhap
+            // 
+            btn_DangNhap.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            btn_DangNhap.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            btn_DangNhap.ForeColor = System.Drawing.Color.Black;
+            btn_DangNhap.Image = global::DoAnCaNhan.Properties.Resources.exit__3_;
+            btn_DangNhap.Location = new System.Drawing.Point(232, 539);
+            btn_DangNhap.Name = "btn_DangNhap";
+            btn_DangNhap.Size = new System.Drawing.Size(156, 43);
+            btn_DangNhap.TabIndex = 13;
+            btn_DangNhap.Text = "Đăng Nhập";
+            btn_DangNhap.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            btn_DangNhap.UseVisualStyleBackColor = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Lime;
+            this.label2.Location = new System.Drawing.Point(242, 465);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(114, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Mật Khẩu: ";
+            // 
+            // txt_TenDangNhap
+            // 
+            this.txt_TenDangNhap.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_TenDangNhap.Location = new System.Drawing.Point(427, 414);
+            this.txt_TenDangNhap.Name = "txt_TenDangNhap";
+            this.txt_TenDangNhap.Size = new System.Drawing.Size(187, 26);
+            this.txt_TenDangNhap.TabIndex = 3;
+            // 
+            // txt_MatKhau
+            // 
+            this.txt_MatKhau.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_MatKhau.Location = new System.Drawing.Point(427, 465);
+            this.txt_MatKhau.Name = "txt_MatKhau";
+            this.txt_MatKhau.Size = new System.Drawing.Size(187, 26);
+            this.txt_MatKhau.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.White;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Lime;
+            this.label3.Location = new System.Drawing.Point(242, 416);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(162, 24);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Tên Đăng Nhập: ";
+            // 
+            // btn_Thoat
+            // 
+            this.btn_Thoat.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btn_Thoat.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Thoat.ForeColor = System.Drawing.Color.Black;
+            this.btn_Thoat.Image = ((System.Drawing.Image)(resources.GetObject("btn_Thoat.Image")));
+            this.btn_Thoat.Location = new System.Drawing.Point(473, 539);
+            this.btn_Thoat.Name = "btn_Thoat";
+            this.btn_Thoat.Size = new System.Drawing.Size(123, 43);
+            this.btn_Thoat.TabIndex = 14;
+            this.btn_Thoat.Text = "Thoát";
+            this.btn_Thoat.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btn_Thoat.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btn_Thoat.UseVisualStyleBackColor = false;
+            // 
+            // ck_HienThiMatKhau
+            // 
+            this.ck_HienThiMatKhau.AutoSize = true;
+            this.ck_HienThiMatKhau.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ck_HienThiMatKhau.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ck_HienThiMatKhau.ForeColor = System.Drawing.Color.Lime;
+            this.ck_HienThiMatKhau.Location = new System.Drawing.Point(640, 470);
+            this.ck_HienThiMatKhau.Margin = new System.Windows.Forms.Padding(2);
+            this.ck_HienThiMatKhau.Name = "ck_HienThiMatKhau";
+            this.ck_HienThiMatKhau.Size = new System.Drawing.Size(74, 19);
+            this.ck_HienThiMatKhau.TabIndex = 18;
+            this.ck_HienThiMatKhau.Text = "Hiển Thị";
+            this.ck_HienThiMatKhau.UseVisualStyleBackColor = false;
+            this.ck_HienThiMatKhau.CheckedChanged += new System.EventHandler(this.ck_HienThiMatKhau_CheckedChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.MenuText;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-11, -3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(965, 620);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.WaitOnLoad = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.label1.Location = new System.Drawing.Point(67, 145);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(148, 31);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "BOOK YOUR";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Arial Narrow", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.label4.Location = new System.Drawing.Point(67, 176);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(93, 31);
+            this.label4.TabIndex = 20;
+            this.label4.Text = "FLIGHT";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Arial Narrow", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.label5.Location = new System.Drawing.Point(67, 207);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(91, 31);
+            this.label5.TabIndex = 21;
+            this.label5.Text = "TODAY";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Algerian", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.Lime;
+            this.label6.Location = new System.Drawing.Point(35, 36);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(864, 45);
+            this.label6.TabIndex = 22;
+            this.label6.Text = "Welcome to our flight booking service";
+            // 
+            // frm_DangNhap
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(930, 606);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ck_HienThiMatKhau);
+            this.Controls.Add(this.btn_Thoat);
+            this.Controls.Add(btn_DangNhap);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txt_MatKhau);
+            this.Controls.Add(this.txt_TenDangNhap);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frm_DangNhap";
+            this.Text = "Login";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_TenDangNhap;
+        private System.Windows.Forms.TextBox txt_MatKhau;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btn_Thoat;
+        private System.Windows.Forms.CheckBox ck_HienThiMatKhau;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ DoAnCaNhan/DoAnCaNhan/frm_DangNhap.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DoAnCaNhan
+{
+    public partial class frm_DangNhap : Form
+    {
+        public frm_DangNhap()
+        {
+            InitializeComponent();
+            //tên đăng nhập
+            var a = this.PointToScreen(label2.Location);
+            a = pictureBox1.PointToClient(a);
+            label2.Parent = pictureBox1;
+            label2.BackColor = Color.Transparent;
+            //mật khẩu
+            var b = this.PointToScreen(label3.Location);
+            b = pictureBox1.PointToClient(b);
+            label3.Parent = pictureBox1;
+            label3.BackColor = Color.Transparent;
+            //hiển thị
+            var c = this.PointToScreen(ck_HienThiMatKhau.Location);
+            c = pictureBox1.PointToClient(c);
+            ck_HienThiMatKhau.Parent = pictureBox1;
+            ck_HienThiMatKhau.BackColor = Color.Transparent;
+            //lb1
+            var d = this.PointToScreen(label1.Location);
+            d = pictureBox1.PointToClient(d);
+            label1.Parent = pictureBox1;
+            label1.BackColor = Color.Transparent;
+            //lb2
+            var h = this.PointToScreen(label4.Location);
+            h = pictureBox1.PointToClient(h);
+            label4.Parent = pictureBox1;
+            label4.BackColor = Color.Transparent;
+            //lb3
+            var l = this.PointToScreen(label5.Location);
+            l = pictureBox1.PointToClient(l);
+            label5.Parent = pictureBox1;
+            label5.BackColor = Color.Transparent;
+            //lb6
+            var e = this.PointToScreen(label6.Location);
+            e = pictureBox1.PointToClient(e);
+            label6.Parent = pictureBox1;
+            label6.BackColor = Color.Transparent;
+        }
+
+        private void ck_HienThiMatKhau_CheckedChanged(object sender, EventArgs e)
+        {
+            if (ck_HienThiMatKhau.Checked == true)
+                txt_MatKhau.UseSystemPasswordChar = false;
+            else txt_MatKhau.UseSystemPasswordChar = true;
+        }
+
+        
+    }
+}
